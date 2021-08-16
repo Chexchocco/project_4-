dd8622df00c338de5b577ec274c62aa63442264c
DESKTOP-QGN7VBL\최용진 <dragon4930@naver.com>
2021-08-16 18:26:56 +0900

    마우스를 올리고 R 을 누르면 일정시간 동안 반대방향으로 움직이는  형식 으로 일단 구현됨

--- Assets/Scripts/TimeObject.cs
+++ Assets/Scripts/TimeObject.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimeObject : MonoBehaviour
+public  class TimeObject : MonoBehaviour
 {
     // Start is called before the first frame update
     private bool mouse_over = false;
     private bool rewind = false;
-    int rewind_durataion = 300;
+    float rewind_durataion = 1.0f;
+    float speed = 0.003f;
+    float cool_down = 0.0f;
     void Start()
     {
         
@@ -19,14 +21,46 @@ public class TimeObject : MonoBehaviour
     {
         if (mouse_over == true)
         {
-                    }
+            if ((Input.GetKey(KeyCode.R)) &&(cool_down <= 0.0f) )
+            {
+                rewind = true;
+                rewind_durataion = 10.0f;
+                cool_down = 3.0f;
+                Debug.Log("rewinded");
+            }
+        }
+        if(rewind = true)
+        {
+            rewind_durataion -= Time.deltaTime;
+
+            if(rewind_durataion <= 0)
+            {
+                rewind = false;
+               
+            }
+        }
+        if(cool_down > 0.0f)
+        {
+            cool_down -= Time.deltaTime;
+        }
+
+        if(rewind == true)
+        {
+            this.transform.Translate(new Vector3(speed, 0, 0));
+        }
+        else
+        {
+            this.transform.Translate(new Vector3(-1*speed, 0, 0));
+        }
+        
+
     }
 
-    void OnMouseOver()
+    private void OnMouseEnter()
     {
         mouse_over = true;
     }
-    void OnMouseExit()
+    private void OnMouseExit()
     {
         mouse_over = false;
     }
