ce7d9d941b00d705887f270f34df987b1970180a
DESKTOP-OM1R0AB\1630d <1630dragon@gmail.com>
2021-08-25 02:32:16 +0900

    짱많이 바꿈

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,22 +11,30 @@ public class Player : MonoBehaviour
     float cool_down = 10.0f;
     private bool isJumping = false;
     SpriteRenderer rend;
+    private bool can_move;
    
     public GameObject recallPrefab;
 
     Vector3 recall_pos;
 
-    //
-
     public float maxSpeed;
     Rigidbody2D rigid;
 
-
     public Transform pTransform;
+
+    //
+    private Animator anime;
+    float recall_pro;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rend = GetComponent<SpriteRenderer>();
+        anime = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
+        can_move = true;
+        recall_pro = 1.0f;
     }
 
 
@@ -38,46 +46,76 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
-
-        //Max Speed
-        if (rigid.velocity.x > maxSpeed) //Right Max Speed
-            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-
-        else if (rigid.velocity.x < maxSpeed * (-1)) //Left Max Speed
-            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
-
-        if ((Input.GetKey(KeyCode.Q)) && (fast_move_count ==3.0f))
-            maxSpeed = 20.0f;
-        if ((Input.GetKey(KeyCode.RightArrow)) && (Input.GetKey(KeyCode.LeftArrow))) 
-        { 
-
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        if(can_move == true)
         {
-            rend.flipX = false;
+            float h = Input.GetAxisRaw("Horizontal");
+            rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+            //Max Speed
+            if (rigid.velocity.x > maxSpeed) //Right Max Speed
+                rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+
+            else if (rigid.velocity.x < maxSpeed * (-1)) //Left Max Speed
+                rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+
+            if ((Input.GetKey(KeyCode.Q)) && (fast_move_count == 3.0f))
+                maxSpeed = 10.0f;
+            if (Input.GetAxisRaw("Horizontal") == 0)
+            {
+                anime.SetBool("Moving", false);
+            }
+            if ((Input.GetKey(KeyCode.RightArrow)) && (Input.GetKey(KeyCode.LeftArrow)))
+            {
+                anime.SetBool("Moving", false);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                rend.flipX = true;
+                anime.SetBool("Moving", true);
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                rend.flipX = false;
+                anime.SetBool("Moving", true);
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+                anime.SetBool("Landing", false);
+                anime.SetTrigger("Jumping");
+            }
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+
+                anime.SetTrigger("Recall");
+                anime.SetBool("Recall_process", true);
+                can_move = false;
+                rigid.gravityScale = 0;
+                rigid.bodyType = RigidbodyType2D.Static;
+            }
         }
-        else if (Input.GetKey(KeyCode.LeftArrow))
+        if(can_move == false)
         {
-            rend.flipX = true;
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Jump();
-        }
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            Recall();
+            recall_pro -= Time.deltaTime;
+            if(recall_pro <= 0)
+            {
+                Recall();
+                can_move = true;
+                rigid.gravityScale = 5;
+                recall_pro = 1.0f;
+                anime.SetBool("Recall_process", false);
+                rigid.bodyType = RigidbodyType2D.Dynamic;
+                
+            }
         }
 
-        if(maxSpeed == 20.0f)
+        if(maxSpeed == 10.0f)
         {
             fast_move_count -= Time.deltaTime ;
             if (fast_move_count <= 0.0f)
             {
                 cool_down = 10.0f;
-                maxSpeed = 10.0f;
+                maxSpeed = 5.0f;
             }
         }
         
@@ -92,6 +130,8 @@ public class Player : MonoBehaviour
 
         recall_trace();
 
+
+
     }
   
     
@@ -101,6 +141,12 @@ public class Player : MonoBehaviour
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(0, 20, 0), ForceMode2D.Impulse);
         }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider != this.GetComponent<BoxCollider2D>())
+        anime.SetBool("Landing", true);
 
     }
     private void OnCollisionStay2D(Collision2D collision)
@@ -114,7 +160,7 @@ public class Player : MonoBehaviour
                 pTransform = collision.gameObject.transform;
 
                 transform.parent = pTransform; //오브젝트의 페어런트를 pTransform으로 지정하여 줍니다.
-
+                
             }
 
         }
